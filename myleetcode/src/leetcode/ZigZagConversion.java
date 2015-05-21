be1a8fd85b218c197c0f05e3d154b57ab457f9f5
capotee <634978538@qq.com>
2015-05-22 01:27:17 +0800

    slow

---
+++ myleetcode/src/leetcode/ZigZagConversion.java
@@ -0,0 +1,131 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ZigZagConversion {
+	
+	public static String convert(String s, int nRows) {
+//		int count = 0;
+//		String swap = s;
+//		String answer = "";
+//		List<String> list = new ArrayList<String>();
+//		while(swap.length() > 0){
+//			if(numRows == 1){
+//				answer = s;
+//				break;
+//			}
+//			if(numRows > 2){
+//				if(swap.length() > numRows){
+//					String sub = swap.substring(0, numRows);
+//					list.add(sub);
+//					count = count + 1;
+//				}else{
+//					String sub = swap;
+//					for(int i = 0 ; i < numRows - swap.length();i++){
+//						sub = sub + " ";
+//					}
+//					list.add(sub);
+//					count = count + 1;
+//					break;
+//				}
+//			
+//				swap = swap.substring(numRows);
+//				int re = 0;
+//			
+//				re = numRows - 2;
+//				
+//				if(swap.length() > re){
+//					String sub2 = swap.substring(0, re);
+//					String reverse2 = "";
+//					for(int r = sub2.length() ; r > 0; r--){
+//						reverse2 = reverse2 + sub2.charAt(r-1);
+//					}
+//					reverse2 = " " + reverse2 + " ";
+//					list.add(reverse2);
+//					count = count + 1;
+//				}else{
+//					String sub2 = swap;
+//					String reverse2 = "";
+//					for(int r = sub2.length()-1 ; r >= 0; r--){
+//						reverse2 = reverse2 + sub2.charAt(r);
+//					}
+//					reverse2 = reverse2 + " ";
+//					for(int p = 0; p < numRows - 1 - swap.length();p++){
+//						reverse2 = " " + reverse2 ;
+//					}
+//					
+//					list.add(reverse2);
+//					count = count + 1;
+//					break;
+//					
+//				}
+//				swap = swap.substring(numRows - 2);
+//			}
+//			
+//			if(numRows == 2){
+//				if(swap.length() > numRows){
+//					String sub = swap.substring(0, numRows);
+//					list.add(sub);
+//					count = count + 1;
+//					swap = swap.substring(numRows);
+//				}else{
+//					String sub = swap;
+//					list.add(sub+" ");
+//					count = count + 1;
+//					break;
+//				}
+//			}
+//			
+//			
+//		}
+//		System.out.println(list);
+//		
+//		for(int z = 0 ; z < numRows; z++){
+//			for(int x=0;x<count;x++){
+//				answer =answer + list.get(x).charAt(z);
+//			}
+//			
+//		}
+//		answer = answer.replace(" ", "");
+//		System.out.println(list);
+//		System.out.println(answer);
+//		return answer;
+		if(nRows <=1)
+            return s;
+
+       StringBuilder[] r = new StringBuilder[nRows];
+       for( int i=0; i<nRows; i++ )
+           r[i] = new StringBuilder();
+
+        int base = nRows+nRows-2;
+        char[] toks = s.toCharArray();
+        for(int i = 0; i < toks.length; i++)
+        {
+            int t = i % base;
+            if(t < nRows)
+            {
+                r[t].append(toks[i]);
+            }
+            else
+            {
+                r[base-t].append(toks[i]);
+            }
+        }
+
+        for( int i = 1; i<nRows; i++ )
+            r[0].append( r[i] );
+        return r[0].toString();
+	}
+	
+	public static void main(String[] args) {
+		
+		System.out.println(convert("ABCD",1));
+		String n = "  " + 1;
+		System.out.println(n);
+		//P   A   H   N
+		//A P L S I I G
+		//Y   I   R
+		
+	}
+}
