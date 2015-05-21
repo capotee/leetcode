a29df912555421083b9ca1f26339d7262bbb1d0d
capotee <634978538@qq.com>
2015-05-22 01:30:13 +0800

    Accepted

--- myleetcode/src/leetcode/PalindromeNumber.java
+++ myleetcode/src/leetcode/PalindromeNumber.java
@@ -2,25 +2,22 @@ package leetcode;
 
 public class PalindromeNumber {
 	public static boolean isPalindrome(int x) {
-        String swap = x+"";
-        int count = swap.length() / 2;
-        if(swap.length() % 2 != 0){
-        	swap = swap.substring(0, count) + swap.substring(count+1, swap.length());
-        	
-        }
-        
-        boolean flag = true;
-        for(int i = 0; i < count; i++){
-        	
-        	if(swap.charAt(i) != swap.charAt(swap.length()-1-i)){
-        		flag = false;
-        		break;
-        	}
-        	
-        }
-		
+		String swap = x + "";
+		int count = swap.length() / 2;
+		if (swap.length() % 2 != 0) {
+			swap = swap.substring(0, count)
+					+ swap.substring(count + 1, swap.length());
+		}
+		boolean flag = true;
+		for (int i = 0; i < count; i++) {
+			if (swap.charAt(i) != swap.charAt(swap.length() - 1 - i)) {
+				flag = false;
+				break;
+			}
+		}
 		return flag;
-    }
+	}
+
 	public static void main(String[] args) {
 		System.out.println(isPalindrome(122241));
 	}
